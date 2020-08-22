6a072b03ceb6c9e6538345a601c037e6d8a7480b
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-08-22 19:20:52 +0530

    👨🏻‍💻 use ejs

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const config = require("./config/config");
 
 app.use(express.static(path.join(__dirname, "static")));
 
-// app.get("/", function (req, res) {
-//   res.send("hello world");
-// });
+app.set("view engine", "ejs");
 
-app.listen(5000, function (err) {
+app.get("/", function (req, res) {
+  const chitters = [
+    {
+      _id: 123,
+      chitter: "Attending workshop :)",
+      comments: [
+        {
+          _id: 12345678,
+          comment: "first comment!",
+          username: "arpancodes"
+        }
+      ],
+      username: "arpancodes",
+    },
+    {
+      _id: 124,
+      chitter: "Lorem ipsum:)",
+      comments: [],
+      username: "arpancodes1",
+    },
+    {
+      _id: 125,
+      chitter: "Hello world :)",
+      comments: [],
+      username: "arpancodes3",
+    },
+  ];
+  res.render("index.ejs", { title: "Chitter | A twitter clone", chitters });
+});
+
+app.listen(config.PORT, function (err) {
   if (err) {
     return console.log("An error occurred.");
   }
-  console.log("The server is running on port 5000");
+  console.log("The server is running on port: " + config.PORT);
 });
